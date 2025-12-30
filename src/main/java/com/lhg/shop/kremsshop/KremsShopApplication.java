6b34bc3bc4b2fcba24357335eb8408d1982dd6d9
HOGUN00 <7080rjs@gmail.com>
2025-12-30 09:27:36 +0900

    init: 기본 설정

---
+++ src/main/java/com/lhg/shop/kremsshop/KremsShopApplication.java
@@ -0,0 +1,13 @@
+package com.lhg.shop.kremsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KremsShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KremsShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhg/shop/kremsshop/KremsShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhg.shop.kremsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KremsShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
